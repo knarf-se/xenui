0aa5346b58c877116cd77aad918af2c6b36a8165
Frank Eriksson <git@knarf.se>
2012-07-11 00:29:04 +0200

    The server now provides indexes for directoies
    Yay! Now the directories is 1337% more navigateable!

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ http.createServer(function ( req, resp ) {
 		if (err === null && stats.isFile() ) {
 			streamFile(resp, fileName);
 		} else {
+			if (err === null && stats.isDirectory()) {
+				return ls(resp, fileName);
+			};
 			fileName += 'index.html';
 			fs.stat(fileName, function ( err, stats ) {
 				if (err === null && stats.isFile() ) {
 					streamFile(resp, fileName);
 				} else {
+					if (err === null && stats.isDirectory()) {
+						return ls(resp, fileName);
+					};
 					resp.writeHead(404);
 					resp.end('File Not Found');
 				}
@@ -36,6 +42,37 @@ function streamFile( dest, fileName ) {
 
 	dest.writeHead(200, { 'Content-Type': mimeType });
 	stream.pipe(dest);
+}
+
+function ls( out, dir ) {
+	fs.readdir(dir, function ( err, files ) {
+		if(err === null) {
+			out.writeHead(300, { 'Content-Type': 'html' });
+			out.write('<!DOCTYPE html5><html><head><meta charset="utf-8"/>'+
+				'<title>Viewing '+path+'</title></head><body><table><th><td>'+
+				'file</td><td>mime</td></th><tr><td><a href="../">Parent '+
+				'Directory</a></td></tr>');
+			for (var i = files.length - 1; i >= 0; i--) {
+				var fsnode = dir+files[i];
+				//	the first sin of node.js -- I know..
+				stats = fs.statSync(dir+files[i]);
+				if(err!==null)return console.log(err);
+				var bs = path.basename(files[i]);
+				console.log(fsnode);
+				if(stats.isDirectory()) bs += '/';
+				out.write('<tr><td><a href="'+bs+'">'+bs+'</a></td>');
+				if(stats.isDirectory()) {
+					out.write('<td> [Directory Entry] </td></tr>');
+				} else {
+					out.write('<td>'+getMime(files[i])+'</td></tr>');
+				}
+			}
+			out.end('</table></body></html>');
+		} else {
+			out.writeHead(500);
+			out.end('Uh, oh! Something bad happend, sorry! ;-C');
+		}
+	});
 }
 
 function getMime( file ) {
