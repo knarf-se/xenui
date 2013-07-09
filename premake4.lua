52227e2283c7c53c195296de2518998a8a39d0fa
Frank M. Eriksson <git@knarf.se>
2013-07-10 00:26:15 +0200

    Build script stuff

--- premake4.lua
+++ premake4.lua
@@ -17,5 +17,23 @@ configuration "Debug"
 	flags { "Symbols" }
 
 project "XenUI"
-	links { "xcb" }
-	files { "./Source/**.*", "./Example/**.*", "./Adaptor/XCB/**.*" }
+	files { "./Source/**.*", "./Example/**.*" }
+	--[[ Doesn't work?
+	if os.findlib("xcb") ~= nil then
+	]]--
+	if os.execute("locate libxcb.so > /dev/null") == 0 then
+		print("\27[32mFound \27[36mXCB\27[0m")
+		defines { "HAVE_XCB" }
+		links { "xcb" }
+		files { "./Adaptor/XCB/**.*" }
+	else
+	--[[ Uncomment when SFML2 target is aviable
+	if os.findlib("sfml-graphics") then
+		print("Found SFML2")
+		defines { "HAVE_SFML2" }
+		links { "sfml-graphics" }
+		files { "./Adaptor/SFML2/**.*" }
+	]]--
+		print("No aviable configurations, \27[34mTERMINATING\27[0m...")
+		os.exit(1)
+	end
