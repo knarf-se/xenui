5ac7797edd24cc7c473df2661acd780167d1b584
Frank M. Eriksson <git@knarf.se>
2013-11-18 10:02:34 +0100

    More work on the buildsystem I really want to have a good and flexible buildsystem in place.

--- Tools/configure.lua
+++ Tools/configure.lua
@@ -1,8 +1,31 @@
 
-if os.execute("locate libxcb.so > /dev/null") == 0 then
-	print("\27[32mFound \27[36mXCB\27[0m")
-	found_xcb = true
-else
+
+--	List of aviable adaptors
+local adaptors = {
+	sfml	=	'sfml-graphics',
+	xcb		=	'xcb',
+}--	Last one will be the default
+
+--	check for library and print xmas-tree ;-)
+function findlib(name)
+	io.write("\27[33mChecking for \27[36m"..name.."\27[0m .. ")
+	if os.execute("locate lib"..name..".so > /dev/null") == 0 then
+		print("\27[32mFound\27[0m")
+		return true
+	else
+		print("\27[31mMissing\27[0m")
+		return false
+	end
+end
+
+adaptor = "none"
+for lib in pairs(adaptors) do
+	local found = findlib(lib)
+	if found then adaptor = lib end
+end
+if adaptor == "none" then
 	print("No aviable configurations, \27[34mTERMINATING\27[0m...")
 	os.exit(1)
+else
+	print("\27[32mUsing \27[36m"..adaptor.."\27[0m")
 end
--- premake4.lua
+++ premake4.lua
@@ -5,12 +5,9 @@ solution "XenUI"
 	language "C++"
 	targetdir ( "./bin" )
 	includedirs { "./Include/" }
-
 	configurations { "Release", "Debug" }
-	if found_xcb == true then
-		print("\27[32mUsing \27[36mXCB\27[0m")
-		defines { "USES_XCB" }
-	end
+	if adaptor == 'xcb'  then defines { "USES_XCB" }  end
+	if adaptor == 'sfml' then defines { "USES_SFML" } end
 
 configuration "Release"
 	defines { "NDEBUG" }
@@ -20,24 +17,25 @@ configuration "Debug"
 	defines { "_DEBUG" }
 	flags { "Symbols" }
 
-project "XenUI"
-	 kind "SharedLib"
-	files { "./Source/Core/**.*" }
-	--[[ Doesn't work?
-	if os.findlib("xcb") ~= nil then
-	]]--
-	if found_xcb then
-		links { "xcb" }
-		files { "./Source/XCB/**.*" }
-	end
-	--[[ Uncomment when SFML2 target is aviable
-	if os.findlib("sfml-graphics") then
-		print("Found SFML2")
-		defines { "HAVE_SFML2" }
-		links { "sfml-graphics" }
-		files { "./Adaptor/SFML2/**.*" }
-	]]--
+--	Libraries
+project "adaptor"
+	kind "StaticLib"
+	if adaptor == 'xcb'		then files "./Source/XCB/**.*"	 end
+	if adaptor == 'sfml'	then files "./Source/SFML2/**.*" end
 
+project "XenUI"
+	kind "SharedLib"
+	files { "./Source/Core/**.*" }
+	links { "adaptor" }
+
+--	Tools
+project "introspect"
+	kind "ConsoleApp"
+	links "XenUI"
+	files { "./Tools/introspect.c" }
+	postbuildcommands { "cp Tools/xintrospect bin/xintrospect" }
+
+--	Samples
 project "Dummy"
 	kind "ConsoleApp"
 	links "XenUI"
